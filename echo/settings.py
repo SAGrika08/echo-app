a2c09cd3c746799882921c70c5c5f706dc08f66b
Sagrika Trehan <trehansagrika0897@gmail.com>
2026-03-03 17:46:10 +0000

    commit

--- echo/settings.py
+++ echo/settings.py
@@ -120,6 +120,9 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+LOGIN_REDIRECT_URL = 'scene-index'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
